9815a3e6e15335bd44103d1ae9a8d0594960fd12
Saitama <aseriouspunch@gmail.com>
2022-10-04 15:37:51 -0700

    Use mix release

---
+++ config/runtime.exs
@@ -0,0 +1,25 @@
+import Config
+
+config :ocap_rpc, :btc,
+  conn: %{
+    hostname: System.get_env("BTC_RPC_HOST", "localhost"),
+    port: System.get_env("BTC_RPC_PORT", "8332"),
+    user: System.get_env("BTC_PRC_USER", ""),
+    password: System.get_env("BTC_RPC_PASSWORD", "")
+  },
+  timeout: System.get_env("BTC_RPC_TIMEOUT", "5000") |> String.to_integer()
+
+config :ocap_rpc, :eth,
+  conn: %{
+    hostname: System.get_env("ETH_RPC_HOST", "localhost"),
+    port: System.get_env("ETH_RPC_PORT", "8545")
+  },
+  timeout: System.get_env("ETH_RPC_TIMEOUT", "5000") |> String.to_integer(),
+  chain_id: System.get_env("ETH_CHAIN_ID", "1") |> String.to_integer()
+
+config :ocap_rpc, :ipfs,
+  conn: %{
+    hostname: System.get_env("IPFS_RPC_HOST", "localhost"),
+    port: System.get_env("IPFS_RPC_PORT", "5001")
+  },
+  timeout: System.get_env("IPFS_RPC_TIMEOUT", "5000") |> String.to_integer()
--- lib/ocap_rpc/application.ex
+++ lib/ocap_rpc/application.ex
@@ -7,53 +7,6 @@ defmodule OcapRpc.Application do
     children = []
     opts = [strategy: :one_for_one, name: OcapRpc.Supervisor]
 
-    update_config()
-
     Supervisor.start_link(children, opts)
-  end
-
-  defp update_config do
-    app = :ocap_rpc
-    update_conn(app, :btc)
-    update_conn(app, :eth)
-    update_conn(app, :ipfs)
-  end
-
-  defp update_conn(app, :btc = type) do
-    conf = Application.get_env(app, type, [])
-
-    conn =
-      conf
-      |> Access.get(:conn)
-      |> Access.get(:conn, %{port: 8332})
-      |> Map.put(:hostname, System.get_env("BTC_RPC_HOST") || "localhost")
-      |> Map.put(:user, System.get_env("BTC_RPC_USER"))
-      |> Map.put(:password, System.get_env("BTC_RPC_PASS"))
-
-    Application.put_env(app, type, Keyword.put(conf, :conn, conn))
-  end
-
-  defp update_conn(app, :eth = type) do
-    conf = Application.get_env(app, type, [])
-
-    conn =
-      conf
-      |> Access.get(:conn)
-      |> Access.get(:conn, %{port: 8545})
-      |> Map.put(:hostname, System.get_env("ETH_RPC_HOST") || "localhost")
-
-    Application.put_env(app, type, Keyword.put(conf, :conn, conn))
-  end
-
-  defp update_conn(app, :ipfs = type) do
-    conf = Application.get_env(app, type, [])
-
-    conn =
-      conf
-      |> Access.get(:conn)
-      |> Access.get(:conn, %{ort: 5001})
-      |> Map.put(:hostname, System.get_env("IPFS_RPC_HOST") || "localhost")
-
-    Application.put_env(app, type, Keyword.put(conf, :conn, conn))
   end
 end
